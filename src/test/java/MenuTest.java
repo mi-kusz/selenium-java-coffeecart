7135a1f7946c63cda07d19d7791da9e28fa3b44a
Michał Kuszewski <michal.kuszewski09@gmail.com>
2025-08-07 10:31:05 +0200

    Add tests for ordering coffee

--- src/test/java/MenuTest.java
+++ src/test/java/MenuTest.java
@@ -46,6 +46,8 @@ public class MenuTest
             "半拿铁"
     );
 
+    private static final int itemsToPromo = 3;
+
     @BeforeAll
     public static void setupClass()
     {
@@ -200,6 +202,15 @@ public class MenuTest
         WebElement promo = getPromoElement();
 
         return promo.findElements(By.cssSelector("div.buttons button")).get(1);
+    }
+
+    private void hoverOverPayButton()
+    {
+        WebElement payButton = getPayButton();
+
+        new Actions(webDriver)
+                .moveToElement(payButton)
+                .perform();
     }
 
     @Test
@@ -401,11 +412,7 @@ public class MenuTest
     @Test
     public void testCartPreviewDoesNotShowUpWhenCartIsEmpty()
     {
-        WebElement payButton = getPayButton();
-
-        new Actions(webDriver)
-                .moveToElement(payButton)
-                .perform();
+        hoverOverPayButton();
 
         assertThrows(NoSuchElementException.class, this::getCartPreview);
     }
@@ -414,16 +421,13 @@ public class MenuTest
     public void testOrderedElementsShowUpInCartPreview()
     {
         List<WebElement> cupElements = getMenuEntries().stream().map(this::getEntryCup).toList();
-        WebElement payButton = getPayButton();
 
         for (WebElement cup : cupElements)
         {
             cup.click();
         }
 
-        new Actions(webDriver)
-                .moveToElement(payButton)
-                .perform();
+        hoverOverPayButton();
 
         List<WebElement> cartPreviewEntries = getCartPreviewEntries();
 
@@ -443,16 +447,13 @@ public class MenuTest
     public void testPlusAndMinusButtonsAreDisplayedInCartPreview()
     {
         List<WebElement> cupElements = getMenuEntries().stream().map(this::getEntryCup).toList();
-        WebElement payButton = getPayButton();
 
         for (WebElement cup : cupElements)
         {
             cup.click();
         }
 
-        new Actions(webDriver)
-                .moveToElement(payButton)
-                .perform();
+        hoverOverPayButton();
 
         List<WebElement> cartPreviewEntries = getCartPreviewEntries();
 
@@ -470,16 +471,13 @@ public class MenuTest
     public void testPlusAndMinusButtonsAddAndRemoveElementsFromCart()
     {
         List<WebElement> cupElements = getMenuEntries().stream().map(this::getEntryCup).toList();
-        WebElement payButton = getPayButton();
 
         for (WebElement cup : cupElements)
         {
             cup.click();
         }
 
-        new Actions(webDriver)
-                .moveToElement(payButton)
-                .perform();
+        hoverOverPayButton();
 
         List<WebElement> cartPreviewEntries = getCartPreviewEntries();
 
@@ -504,16 +502,13 @@ public class MenuTest
     public void testRemovingSingleElementsFromPreviewDeleteEntry()
     {
         List<WebElement> cupElements = getMenuEntries().stream().map(this::getEntryCup).toList();
-        WebElement payButton = getPayButton();
 
         for (WebElement cup : cupElements)
         {
             cup.click();
         }
 
-        new Actions(webDriver)
-                .moveToElement(payButton)
-                .perform();
+        hoverOverPayButton();
 
         List<WebElement> cartPreviewEntries = getCartPreviewEntries();
         int expectedEntries = cartPreviewEntries.size();
@@ -554,7 +549,7 @@ public class MenuTest
 
         for (WebElement menuEntry : menuEntries)
         {
-            if (counter != 0 && counter % 3 == 0)
+            if (counter != 0 && counter % itemsToPromo == 0)
             {
                 assertDoesNotThrow(this::getPromoElement);
             }
@@ -570,16 +565,27 @@ public class MenuTest
         }
     }
 
-    @Test
-    public void testAcceptAndDiscardPromoButtonsAreDisplayed()
+    private BigDecimal addItemsToCartToShowPromo()
     {
         WebElement menuEntry = getMenuEntries().getFirst();
         WebElement cupElement = getEntryCup(menuEntry);
+        BigDecimal entryPrice = getEntryPrice(menuEntry);
 
-        for (int i = 0; i < 3; ++i)
+        BigDecimal expectedPrice = BigDecimal.ZERO;
+
+        for (int i = 0; i < itemsToPromo; ++i)
         {
+            expectedPrice = expectedPrice.add(entryPrice);
             cupElement.click();
         }
+
+        return expectedPrice;
+    }
+
+    @Test
+    public void testAcceptAndDiscardPromoButtonsAreDisplayed()
+    {
+        addItemsToCartToShowPromo();
 
         WebElement acceptButton = getAcceptPromoButton();
         WebElement discardButton = getDiscardPromoButton();
@@ -591,17 +597,7 @@ public class MenuTest
     @Test
     public void testDiscardPromoButtonDoesNotChangeAnything()
     {
-        WebElement menuEntry = getMenuEntries().getFirst();
-        WebElement cupElement = getEntryCup(menuEntry);
-        BigDecimal entryPrice = getEntryPrice(menuEntry);
-
-        BigDecimal expectedPrice = BigDecimal.ZERO;
-
-        for (int i = 0; i < 3; ++i)
-        {
-            expectedPrice = expectedPrice.add(entryPrice);
-            cupElement.click();
-        }
+        BigDecimal expectedPrice = addItemsToCartToShowPromo();
 
         WebElement discardButton = getDiscardPromoButton();
         discardButton.click();
@@ -610,7 +606,406 @@ public class MenuTest
         assertPriceOnButtonIsEqual(expectedPrice);
     }
 
-    // TODO: Accept promo adds discounted mocha (price is 4)
-    // TODO: Discounted item is always on top of the preview
-    // TODO: Discounted item could be removed, but not added
+    @Test
+    public void testAcceptPromoButtonAddsPrice()
+    {
+        BigDecimal discountedPrice = new BigDecimal("4.00");
+        BigDecimal expectedPrice = addItemsToCartToShowPromo();
+
+        WebElement acceptButton = getAcceptPromoButton();
+        acceptButton.click();
+
+        expectedPrice = expectedPrice.add(discountedPrice);
+
+        assertPriceOnButtonIsEqual(expectedPrice);
+    }
+
+    @Test
+    public void testAcceptPromoAddsDiscountedMochaToPreviewOnTheFirstPlace()
+    {
+        addItemsToCartToShowPromo();
+
+        WebElement acceptButton = getAcceptPromoButton();
+        acceptButton.click();
+
+        hoverOverPayButton();
+
+        WebElement cartPreviewFirstEntry = getCartPreviewEntries().getFirst();
+        String entryName = getCartPreviewEntryName(cartPreviewFirstEntry);
+
+        assertEquals("(Discounted) Mocha", entryName);
+    }
+
+    private boolean isSorted(List<String> strings)
+    {
+        for (int i = 1; i < strings.size(); ++i)
+        {
+            if (strings.get(i - 1).compareTo(strings.get(i)) > 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Test
+    public void testItemsInCartAreSortedAlphabetically()
+    {
+        List<WebElement> cupElements = getMenuEntries().stream().map(this::getEntryCup).toList();
+
+        for (WebElement cup : cupElements)
+        {
+            cup.click();
+        }
+
+        hoverOverPayButton();
+
+        List<String> cartPreviewEntryNames = getCartPreviewEntries().stream().map(this::getCartPreviewEntryName).toList();
+
+        assertTrue(isSorted(cartPreviewEntryNames));
+    }
+
+    @Test
+    @Disabled("I think if a user gets a discounted item for ordering 3 items, the user should not be allowed to increase the number of discounted items without limit.")
+    public void testDiscountedItemsCannotBeAddedInCartPreview()
+    {
+        addItemsToCartToShowPromo();
+
+        WebElement acceptPromoButton = getAcceptPromoButton();
+        acceptPromoButton.click();
+
+        hoverOverPayButton();
+
+        WebElement discountedEntry = getCartPreviewEntries().getFirst();
+        assertThrows(NoSuchElementException.class, () -> getAddButton(discountedEntry));
+    }
+
+    @Test
+    public void testDiscountedItemsCanBeRemovedInCartPreview()
+    {
+        addItemsToCartToShowPromo();
+
+        WebElement acceptPromoButton = getAcceptPromoButton();
+        acceptPromoButton.click();
+
+        hoverOverPayButton();
+
+        List<WebElement> cartPreviewEntries = getCartPreviewEntries();
+        int initialCartPreviewSize = cartPreviewEntries.size();
+
+        WebElement discountedEntry = cartPreviewEntries.getFirst();
+        WebElement removeButton = getRemoveButton(discountedEntry);
+        removeButton.click();
+
+        cartPreviewEntries = getCartPreviewEntries();
+
+        assertEquals(initialCartPreviewSize - 1, cartPreviewEntries.size());
+    }
+
+    @Test
+    public void testPromoShowsUpEveryThreeBasicItemsOrderedWithoutPromoItems()
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            addItemsToCartToShowPromo();
+
+            assertTrue(getPromoElement().isDisplayed());
+        }
+    }
+
+    @Test
+    @Disabled("Discounted items counts to the promo counter. Example: 3 basic items -> Get promo item -> Need to order 2 (instead of 3) another items to get another promo.")
+    public void testPromoShowsUpEveryThreeBasicItemsOrderedWithPromoItems()
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            addItemsToCartToShowPromo();
+
+            assertTrue(getPromoElement().isDisplayed());
+
+            getAcceptPromoButton().click();
+        }
+    }
+
+    @Test
+    @Disabled("There is no upper limit for discounted items")
+    public void testNumberOfDiscountedItemsIsLimitedByNumberOfBasicItems()
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            addItemsToCartToShowPromo();
+            getAcceptPromoButton().click();
+        }
+
+        // Currently in cart: 9 basic items and 3 discounted
+
+        hoverOverPayButton();
+
+        List<WebElement> cartPreviewEntries = getCartPreviewEntries();
+        WebElement discountedItemEntry = cartPreviewEntries.getFirst();
+        WebElement basicItemEntry = cartPreviewEntries.get(1);
+
+        WebElement removeButton = getRemoveButton(basicItemEntry);
+
+        int basicItemCount = getCartPreviewEntryCount(basicItemEntry);
+        int discountedItemCount;
+        int maximumOfDiscountedItems;
+
+        while (basicItemCount > 0)
+        {
+            maximumOfDiscountedItems = basicItemCount / itemsToPromo;
+
+            if (maximumOfDiscountedItems == 0)
+            {
+                assertThrows(StaleElementReferenceException.class, () -> getCartPreviewEntryCount(discountedItemEntry));
+                break;
+            }
+            else
+            {
+                discountedItemCount = getCartPreviewEntryCount(discountedItemEntry);
+            }
+
+            assertTrue(discountedItemCount <= maximumOfDiscountedItems);
+
+            removeButton.click();
+
+            --basicItemCount;
+        }
+    }
+
+    private WebElement getModalElement()
+    {
+        return webDriver.findElement(By.cssSelector("div.modal-content"));
+    }
+
+    private WebElement getModalNameInput()
+    {
+        WebElement modal = getModalElement();
+
+        return modal.findElement(By.cssSelector("input#name"));
+    }
+
+    private WebElement getModalEmailInput()
+    {
+        WebElement modal = getModalElement();
+
+        return modal.findElement(By.cssSelector("input#email"));
+    }
+
+    private WebElement getModalPromotionCheckbox()
+    {
+        WebElement modal = getModalElement();
+
+        return modal.findElement(By.cssSelector("input#promotion"));
+    }
+
+    private WebElement getSubmitPaymentButton()
+    {
+        WebElement modal = getModalElement();
+
+        return modal.findElement(By.cssSelector("button#submit-payment"));
+    }
+
+    @Test
+    public void testModalIsNotDisplayedInitially()
+    {
+        WebElement modal = getModalElement();
+
+        assertFalse(modal.isDisplayed());
+    }
+
+    @Test
+    public void testModalShowsUpAfterPayButtonClick()
+    {
+        WebElement payButton = getPayButton();
+        WebElement modal = getModalElement();
+
+        payButton.click();
+
+        assertTrue(modal.isDisplayed());
+    }
+
+    @Test
+    public void testModalNameInputIsDisplayed()
+    {
+        WebElement payButton = getPayButton();
+        payButton.click();
+
+        WebElement nameInput = getModalNameInput();
+
+        assertTrue(nameInput.isDisplayed());
+    }
+
+    @Test
+    public void testModalEmailInputIsDisplayed()
+    {
+        WebElement payButton = getPayButton();
+        payButton.click();
+
+        WebElement emailInput = getModalEmailInput();
+
+        assertTrue(emailInput.isDisplayed());
+    }
+
+    @Test
+    public void testModalPromoInputIsDisplayed()
+    {
+        WebElement payButton = getPayButton();
+        payButton.click();
+
+        WebElement promoCheckbox = getModalPromotionCheckbox();
+
+        assertTrue(promoCheckbox.isDisplayed());
+    }
+
+    @Test
+    public void testModalSubmitInputIsDisplayed()
+    {
+        WebElement payButton = getPayButton();
+        payButton.click();
+
+        WebElement submitButton = getSubmitPaymentButton();
+
+        assertTrue(submitButton.isDisplayed());
+    }
+
+    @Test
+    public void testModalNameAndEmailCannotBeEmpty()
+    {
+        WebElement payButton = getPayButton();
+        payButton.click();
+
+        WebElement submitButton = getSubmitPaymentButton();
+        submitButton.click();
+
+        // Modal does not disappear
+        assertTrue(getModalElement().isDisplayed());
+    }
+
+    @Test
+    public void testModalEmailCannotBeEmpty()
+    {
+        WebElement payButton = getPayButton();
+        payButton.click();
+
+        WebElement nameInput = getModalNameInput();
+        nameInput.sendKeys("Test name");
+
+        WebElement submitButton = getSubmitPaymentButton();
+        submitButton.click();
+
+        // Modal does not disappear
+        assertTrue(getModalElement().isDisplayed());
+    }
+
+    @Test
+    public void testModalNameCannotBeEmpty()
+    {
+        WebElement payButton = getPayButton();
+        payButton.click();
+
+        WebElement emailInput = getModalEmailInput();
+        emailInput.sendKeys("test@test.com");
+
+        WebElement submitButton = getSubmitPaymentButton();
+        submitButton.click();
+
+        // Modal does not disappear
+        assertTrue(getModalElement().isDisplayed());
+    }
+
+    @Test
+    public void testModalEmailMustBeValid()
+    {
+        WebElement payButton = getPayButton();
+        payButton.click();
+
+        WebElement nameInput = getModalNameInput();
+        nameInput.sendKeys("Test name");
+
+        WebElement emailInput = getModalEmailInput();
+        emailInput.sendKeys("test");
+
+        WebElement submitButton = getSubmitPaymentButton();
+        submitButton.click();
+
+        // Modal does not disappear
+        assertTrue(getModalElement().isDisplayed());
+    }
+
+    @Test
+    public void testModalDisappearsWithValidData()
+    {
+        WebElement payButton = getPayButton();
+        payButton.click();
+
+        WebElement nameInput = getModalNameInput();
+        nameInput.sendKeys("Test name");
+
+        WebElement emailInput = getModalEmailInput();
+        emailInput.sendKeys("test@test.com");
+
+        WebElement submitButton = getSubmitPaymentButton();
+        submitButton.click();
+
+        // Modal does not disappear
+        assertFalse(getModalElement().isDisplayed());
+    }
+
+    @Test
+    public void testModalCheckboxCanBeSelectedAndUnselected()
+    {
+        WebElement payButton = getPayButton();
+        payButton.click();
+
+        WebElement checkboxInput = getModalPromotionCheckbox();
+
+        assertFalse(checkboxInput.isSelected());
+
+        checkboxInput.click();
+        assertTrue(checkboxInput.isSelected());
+
+        checkboxInput.click();
+        assertFalse(checkboxInput.isSelected());
+    }
+
+    private WebElement getSnackbarElement()
+    {
+        return webDriver.findElement(By.cssSelector("div.snackbar.success"));
+    }
+
+    @Test
+    public void testSnackbarIsNotDisplayedInitially()
+    {
+        assertThrows(NoSuchElementException.class, this::getSnackbarElement);
+    }
+
+    @Test
+    public void testSnackBarShowsUpAfterPurchase()
+    {
+        getPayButton().click();
+
+        getModalNameInput().sendKeys("Test name");
+        getModalEmailInput().sendKeys("test@test.com");
+        getSubmitPaymentButton().click();
+
+        assertTrue(getSnackbarElement().isDisplayed());
+    }
+
+    @Test
+    public void testSnackbarDisappearsAfterTime()
+    {
+        getPayButton().click();
+
+        getModalNameInput().sendKeys("Test name");
+        getModalEmailInput().sendKeys("test@test.com");
+        getSubmitPaymentButton().click();
+
+        WebElement snackbar = getSnackbarElement();
+
+        wait.until(ExpectedConditions.invisibilityOf(snackbar));
+
+        assertFalse(snackbar.isDisplayed());
+    }
 }
